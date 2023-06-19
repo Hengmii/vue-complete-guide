a2670cd51188d868099e066a4c0169403aae46f4
Hengmi <hengmii@hotmail.com>
2023-06-19 23:27:10 +0800

    Initial commit.

--- 01-getting-started/app.js
+++ 01-getting-started/app.js
@@ -1,52 +1,29 @@
-// const buttonEl = document.querySelector('button')
-// const inputEl = document.querySelector('input')
-// const listEl = document.querySelector('li')
-
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+//
 // function addGoal() {
-//     const enteredValue = inputEl.value;
-//     if (!enteredValue) {
-//         console.error("Empty value");
-//         return;
-//     }
-//     const listItemEl = document.createElement('li')
-//     listItemEl.textContent = enteredValue
-//     listEl.append(listItemEl)
-//     inputEl.value = ''
+// 	console.log('Hi')
+// 	const enteredValue = inputEl.value;
+// 	const listItemEl = document.createElement('li');
+// 	listItemEl.textContent = enteredValue;
+// 	listEl.appendChild(listItemEl);
+// 	inputEl.value = '';
 // }
+// buttonEl.addEventListener('click', addGoal);
 
-// buttonEl.addEventListener('click', addGoal)
 
-
-// VUE 2
-// let app = new Vue({
-//     el: '#app',
-//     data: {
-//         goals: [],
-//         enteredValue: ''
-//     },
-//     methods: {
-//         addGoal() {
-//             this.goals.push(this.enteredValue)
-//             this.enteredValue = ''
-//         }
-//     }
-// })
-
-// VUE 3
 Vue.createApp({
-    data() {
-        return {
-            goals: [],
-            enteredValue: ''
-        }
-    },
-    methods: {
-        addGoal() {
-            goal = this.enteredValue
-            if (goal == '') return;
-
-            this.goals.push(goal);
-            this.enteredValue = ''
-        }
-    }
-}).mount('#app')
+	data() {
+		return {
+			goals: [],
+			enteredValue: ''
+		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredValue);
+			this.enteredValue = '';
+		}
+	}
+}).mount('#app');
--- 02-basic-concepts/01-goals/app.js
+++ 02-basic-concepts/01-goals/app.js
@@ -1,20 +1,21 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            courseGoalA: "Have fun and learn stuff",
-            courseGoalB: "Be the best coder in the whole wide world",
-            // courseGoalB: "<em>Be the best coder in the whole wide world</em>",
-            vueLink: 'https://vuejs.org',
-            message: 'You loaded this page on ' + new Date().toLocaleString()
-        }
-    },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random()
-            return randomNumber < 0.5 ? this.courseGoalA : this.courseGoalB
-        }
-    }
-})
+	data() {
+		return {
+			courseGoalA: 'Finish the course and learn Vue!',
+			courseGoalB: '<h2>Master Vue and build amazing apps!</h2>',
+			vueLink: 'https://vuejs.org/'
+		}
+	},
+	methods: {
+		outputGoal() {
+			const randomNumber = Math.random();
+			if (randomNumber < 0.5) {
+				return this.courseGoalA;
+			} else {
+				return this.courseGoalB;
+			}
+		}
+	}
+});
 
-app.mount('#user-goal')
-
+app.mount('#user-goal');
